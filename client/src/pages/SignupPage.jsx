ab9d8d70af27b64fc0d9894271c2639f5f83496f
samelimill <samelimill@gmail.com>
2024-01-04 19:00:31 -0800

    Filled out cart and cart item components with associated utils

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -47,7 +47,7 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-
+      
       <h2>Login</h2>
       <form onSubmit={handleLoginFormSubmit}>
         <div className="flex-row space-between my-2">
