3e7d487d4361b5ade8b5e64606c2383c1e18e78b
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-04 21:51:23 -0800

    commented out code interferring with server

--- server/server.js
+++ server/server.js
@@ -43,39 +43,39 @@ const startApolloServer = async () => {
 
 startApolloServer();
 
-app.use(express.static(process.env.STATIC_DIR));
+// app.use(express.static(process.env.STATIC_DIR));
 
-app.get("/", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
-  res.sendFile(path);
-});
+// app.get("/", (req, res) => {
+//   const path = resolve(process.env.STATIC_DIR + "/index.html");
+//   res.sendFile(path);
+// });
 
-app.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
-});
+// app.get("/config", (req, res) => {
+//   res.send({
+//     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//   });
+// });
 
-app.post("/create-payment-intent", async (req, res) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: "EUR",
-      amount: 1999,
-      automatic_payment_methods: { enabled: true },
-    });
+// app.post("/create-payment-intent", async (req, res) => {
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       currency: "EUR",
+//       amount: 1999,
+//       automatic_payment_methods: { enabled: true },
+//     });
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (e) {
-    return res.status(400).send({
-      error: {
-        message: e.message,
-      },
-    });
-  }
-});
+//     res.send({
+//       clientSecret: paymentIntent.client_secret,
+//     });
+//   } catch (e) {
+//     return res.status(400).send({
+//       error: {
+//         message: e.message,
+//       },
+//     });
+//   }
+// });
 
-app.listen(5252, () =>
-  console.log(`Node server listening at http://localhost:5252`)
-);
+// app.listen(5252, () =>
+//   console.log(`Node server listening at http://localhost:5252`)
+// );
