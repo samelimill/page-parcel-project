6aec2dab296b24645e01f47093a14bf4fa569e1e
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-08 21:41:21 -0800

    fixed button issue

--- client/src/components/Home/Button.jsx
+++ client/src/components/Home/Button.jsx
@@ -32,7 +32,7 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to='/' className='btn-mobile'>
+    <Link to='/login' className='btn-mobile'>
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
