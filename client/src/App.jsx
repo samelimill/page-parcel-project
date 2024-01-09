60ad9dfc599e0f90f095a9510cf9e484a2678139
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-08 21:41:07 -0800

    created new pathways

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+
 import Navbar from '../src/components/Home/Navbar';
 import './App.css';
 import Home from '../src/pages/HomePage';
-
 import Contact from '../src/components/Contact/Contact';
+import Stripe from '../src/pages/StripePage';
+import Login from './pages/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <Routes>
         <Route path='/' exact element={ <Home />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/stripe' exact element={ <Stripe />}></Route>
+        <Route path='/login' exact element={ <Login />}></Route>
+        <Route path='/signup' exact element={ <Signup />}></Route>
           </Routes>
       </Router>
     </>
@@ -22,4 +28,3 @@ function App() {
 
 
 export default App;
-
