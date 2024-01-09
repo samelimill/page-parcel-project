24cc3f8b23e8e574f5476b458447849bbfc6d27c
viaheather <viaheatherr@gmail.com>
2024-01-08 21:28:36 -0800

    added a few components and updated existing pages

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,25 +1,50 @@
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
+import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
-const Profile = () => {
-    const { profileId } = useParams();
+function AccountPage() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
 
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { profileId: profileId },
-  });
-
-  const profile = data?.getUser || {};
-
-  if (loading) {
-    return <div>Loading...</div>;
+  if (data) {
+    user = data.user;
   }
-  return (
-    <div>
-      <h1>{profile.username}</h1>
-    </div>
-  );
-};
 
-export default Profile;
+  return (
+    <>
+      <div>
+        <Link to="/">← Back to Products</Link>
+
+        {user ? (
+          <>
+            <h2>
+              Order History for {user.firstName} {user.lastName}
+            </h2>
+            {user.orders.map((order) => (
+              <div key={order._id}>
+                <h3>
+                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                </h3>
+                <div>
+                  {order.products.map(({ _id, name, price }, index) => (
+                    <div key={index}>
+                      <Link to={`/products/${_id}`}>
+                        <p>{name}</p>
+                      </Link>
+                      <div>
+                        <span>${price}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+export default AccountPage;
