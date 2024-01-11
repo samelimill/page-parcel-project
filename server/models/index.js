f4ad981cedeb66b23e408de3db7f2d743adb7560
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-10 20:35:08 -0800

    deleted repeated code

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,4 @@ const User = require("./User");
 const Order = require("./Order");
 const Product = require("./Product");
 
-module.exports = { User };
-module.exports = { Order };
-module.exports = { Product };
+module.exports = { User, Order, Product };
