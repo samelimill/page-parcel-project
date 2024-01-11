a5e30f97dc748e157f475ea0239aed9011a054d8
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-10 20:33:32 -0800

    installed apolloprovider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,20 @@ import Stripe from '../src/pages/StripePage';
 import Login from './pages/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Replace with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
+
+
 
 
 function App() {
  
   return (
+    <ApolloProvider client={client}>
     <>
       <Router>
         <Navbar />
@@ -23,6 +32,7 @@ function App() {
           </Routes>
       </Router>
     </>
+    </ApolloProvider>
   );
 }
 
