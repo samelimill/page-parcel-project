1f4e8bc10286407b7a2202d9d4510af446500e77
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-13 23:12:50 -0800

    deleted unwanted code

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -14,36 +14,7 @@ function AccountPage() {
   return (
     <>
       <div>
-      <Link to="/" style={{ textDecoration: 'none', color: 'black', fontSize: '20px' }}>
-        ← Back to Products
-      </Link>
         <Account />
-        {/* {user ? (
-          <>
-            <h2>
-              Order History for {user.firstName} {user.lastName}
-            </h2>
-            {user.orders.map((order) => (
-              <div key={order._id}>
-                <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
-                </h3>
-                <div>
-                  {order.products.map(({ _id, name, price }, index) => (
-                    <div key={index}>
-                      <Link to={`/products/${_id}`}>
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <span>${price}</span>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </>
-        ) : null} */}
       </div>
     </>
   );
